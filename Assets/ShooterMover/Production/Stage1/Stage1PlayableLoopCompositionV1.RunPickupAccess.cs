9e48aa56ce572fdbdbdb73e773ed202d4e137302
YeerooXY <145460159+YeerooXY@users.noreply.github.com>
2026-07-22 11:29:20 +0200

    PICKUP-LIVE-001: narrow shared-run pickup access

--- Assets/ShooterMover/Production/Stage1/Stage1PlayableLoopCompositionV1.RunPickupAccess.cs
+++ Assets/ShooterMover/Production/Stage1/Stage1PlayableLoopCompositionV1.RunPickupAccess.cs
@@ -1,11 +1,7 @@
-using ShooterMover.Application.Holdings;
-using ShooterMover.Application.Missions.Results;
 using ShooterMover.Application.Progression.Experience;
 using ShooterMover.Domain.Common;
 using ShooterMover.TestSupport.VisibleSlice;
-using ShooterMover.UI.ProductionFlow;
 using ShooterMover.UnityAdapters.Missions.Rooms;
-using ShooterMover.UnityAdapters.Weapons.Live;
 
 namespace ShooterMover.UnityAdapters.Production.Stage1
 {
@@ -19,10 +15,8 @@ namespace ShooterMover.UnityAdapters.Production.Stage1
                     && controller != null
                     && controller.PlayerLiveAuthority != null
                     && controller.PlayerLiveAuthority.IsInitialized
+                    && controller.PlayerTransform != null
                     && rooms != null
-                    && missionResults != null
-                    && holdings != null
-                    && effectEmitter != null
                     && experience != null
                     && runStableId != null;
             }
@@ -33,11 +27,6 @@ namespace ShooterMover.UnityAdapters.Production.Stage1
             get { return controller; }
         }
 
-        internal ProductionFlowCoordinatorV1 RunPickupFlow
-        {
-            get { return flow; }
-        }
-
         internal StableId RunPickupRunStableId
         {
             get { return runStableId; }
@@ -46,21 +35,6 @@ namespace ShooterMover.UnityAdapters.Production.Stage1
         internal RoomRuntimeComposition2D RunPickupRooms
         {
             get { return rooms; }
-        }
-
-        internal MissionRunResultAuthorityV1 RunPickupMissionResults
-        {
-            get { return missionResults; }
-        }
-
-        internal PlayerHoldingsService RunPickupHoldings
-        {
-            get { return holdings; }
-        }
-
-        internal InventoryWeaponEffectEmitter2D RunPickupEffectEmitter
-        {
-            get { return effectEmitter; }
         }
 
         internal PlayerExperienceAuthorityV1 RunPickupExperience
