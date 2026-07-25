606eede06a829e6a0b3e7023415c07ea422cde86
YeerooXY <145460159+YeerooXY@users.noreply.github.com>
2026-07-25 19:18:48 +0200

    WEAPON-DATA-002A: preserve mapping-intent parameter order

--- Assets/ShooterMover/Runtime/Application/Weapons/Catalog/WeaponBlueprintMappingContracts.cs
+++ Assets/ShooterMover/Runtime/Application/Weapons/Catalog/WeaponBlueprintMappingContracts.cs
@@ -165,8 +165,8 @@ namespace ShooterMover.Application.Weapons.Catalog
             WeaponCatalogSpreadInterpretation spreadInterpretation,
             int pulsesPerShot,
             double intervalBetweenPulsesSeconds,
-            double intervalAfterBurstSeconds,
             double intervalBetweenBurstShotsSeconds,
+            double intervalAfterBurstSeconds,
             WeaponProjectileKind projectileKind,
             WeaponProjectileTerminationBehavior projectileTermination,
             WeaponDamageCategory? explicitDamageCategory,
@@ -184,8 +184,8 @@ namespace ShooterMover.Application.Weapons.Catalog
             SpreadInterpretation = spreadInterpretation;
             PulsesPerShot = pulsesPerShot;
             IntervalBetweenPulsesSeconds = intervalBetweenPulsesSeconds;
-            IntervalAfterBurstSeconds = intervalAfterBurstSeconds;
             IntervalBetweenBurstShotsSeconds = intervalBetweenBurstShotsSeconds;
+            IntervalAfterBurstSeconds = intervalAfterBurstSeconds;
             ProjectileKind = projectileKind;
             ProjectileTermination = projectileTermination;
             ExplicitDamageCategory = explicitDamageCategory;
@@ -204,8 +204,8 @@ namespace ShooterMover.Application.Weapons.Catalog
         public WeaponCatalogSpreadInterpretation SpreadInterpretation { get; }
         public int PulsesPerShot { get; }
         public double IntervalBetweenPulsesSeconds { get; }
-        public double IntervalAfterBurstSeconds { get; }
         public double IntervalBetweenBurstShotsSeconds { get; }
+        public double IntervalAfterBurstSeconds { get; }
         public WeaponProjectileKind ProjectileKind { get; }
         public WeaponProjectileTerminationBehavior ProjectileTermination { get; }
         public WeaponDamageCategory? ExplicitDamageCategory { get; }
